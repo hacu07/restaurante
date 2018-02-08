d9da39c2787a3b0a7fb2198614cb7975aa3483f0
hacu07 <32797963+hacu07@users.noreply.github.com>
2018-02-08 11:42:50 -0500

    DETALLE PEDIDO CHEF

--- JS/restaurante.js
+++ JS/restaurante.js
@@ -17,6 +17,9 @@ function iniciarSesion(){
 
 //****** MOSTRAR VENTANAS **************
 function mostrarVentanaChef1(){
-	var txt='Hola';
-	$('#contenedor').text(txt);
+	var txt='<div id="cont_norte"> <div id="imagenLogo" class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1"> <div id="imagen"> 	<img src="img/logo1.png" alt="logo" id="imgLogin"></div></div><div id="infoLogo" class="col-xs-7 col-sm-7 col-md-7 col-lg-7 "> 	<div id="logoModulo" class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-4 col-sm-4 col-md-4 col-lg-4"></div><div id="logoNombre" class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>	</div></div><div id="cont_centro"><div class="tbl_cocina"><table class="table table-bordered"> <thead><tr><th scope="col"># Pedido</th><th scope="col">Mesero</th>	<th scope="col">Mesa</th><th scope="col">Estado</th><th scope="col">Ver</th> </tr></thead><tbody>';
+		//Crea filas
+		txt += '<tr><th scope="row">1</th><td>Harold</td><td>Harold</td><td>Harold</td><td><button>Ver Detalles</button></td></tr>';
+		txt += '</tbody></table></div></div>';
+	$('#contenedor').html(txt);
 }
