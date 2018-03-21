2f436d1394d1ed843d4565d5738c7d816726f679
Sena <Sena@IBADFPCII7F477.FormacionSena.RED>
2018-03-21 11:04:48 -0500

    Modulo administrador
    se terminan formularios hasta el btn productos
    falta modificarles css

--- JS/moduloAdmin.js
+++ JS/moduloAdmin.js
@@ -11,8 +11,8 @@ function mostrarModuloAdmin(nombre){
 
 	txt += '<div id="menu" class="col-md-2 col-lg-2 col-sm-2">';
 	txt += '<button type=button id="btnUsuario" class="btn btn-block " onclick=registroNuevosUsuarios()>USUARIO</button>';
-	txt += '<button type=button id="btnCategorias" class="btn btn-block ">CATEGORIAS</button>';
-	txt += '<button type=button id="btnProductos" class="btn btn-block ">PRODUCTOS</button>';
+	txt += '<button type=button id="btnCategorias" class="btn btn-block" onclick=AgregarCategoria()>CATEGORIAS</button>';
+	txt += '<button type=button id="btnProductos" class="btn btn-block "onclick=agregarProducto()>PRODUCTOS</button>';
 	txt += '<button type=button id="btnEstadisticas" class="btn btn-block ">ESTADISTICAS</button>';
 	txt += '</div>';
 
@@ -39,11 +39,11 @@ function registroNuevosUsuarios(){
     var txt1 = '<form method="post">';
     txt1 += '<div>';
     txt1 += 	'<label for="Nombre">Usuario:</label>';
-    txt1 += 	'<input type="text" id="nombre" />';
+    txt1 += 	'<input type="text" id="nombre" placeholder="Escriba Nombre de Usuario" />';
     txt1 += '</div>';
     txt1 += '<div>';
     txt1 += 	'<label for="contrasenia">Contraseña:</label>';
-    txt1 += 	'<input type="password" id="contrasenia" />';
+    txt1 += 	'<input type="password" id="contrasenia" placeholder="Escriba Contraseña" />';
     txt1 += '</div>';
     txt1 += '<div>';
 
@@ -68,7 +68,64 @@ function registroNuevosUsuarios(){
 }
 
   
+function AgregarCategoria(){
 
+	var txt = '<h1>AÑADIR NUEVA CATEGORIA</h1>';
+
+    $("#titulo").html(txt); 
+
+    var txt1 = '<form method="post">';
+    txt1 += '<div>';
+    txt1 += 	'<label for="categoria">Nombre de la categoria:</label>';
+    txt1 += 	'<input type="text" id="nomCategoria" placeholder="Escriba el nombre de la categoria"/>';
+    txt1 += '</form>';
+	
+    $("#trabajoCentro").html(txt1); 
+
+    var txt2 = '<div>';
+    txt2 += '	<button id="btnAgregar" class="btn" type="button">AÑADIR</button>';
+    txt2 += ' </div>';
+
+    $("#trabajoSur").html(txt2); 
+}
+
+  
+function agregarProducto(){
+
+	var txt = '<h1>AÑADIR NUEVO PRODUCTO</h1>';
+
+    $("#titulo").html(txt); 
+
+    var txt1 = '<form method="post">';
+    txt1 += '<div>';
+    txt1 += 	'<label for="producto">Nombre del producto:</label>';
+    txt1 += 	'<input type="text" id="producto" placeholder="Escriba Nombre del producto" />';
+    txt1 += '</div>';
+    txt1 += '<div>';
+    txt1 += 	'<label for="precio">Precio:</label>';
+    txt1 += 	'<input type="number" id="precio" placeholder="Escriba precio el producto" />';
+    txt1 += '</div>';
+    txt1 += '<div>';
+
+    txt1 += '<div class="btn-group">';
+    txt1 += '<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Categoria <span class="glyphicon glyphicon-triangle-bottom"></span></button>';
+    txt1 += '	<div class="dropdown-menu">';
+    txt1 += '	<li><a href="#">PLATOS</a></li>';
+    txt1 += '	<li><a href="#">BEBIDAS</a></li>';
+   txt1 += '</div>';   
+   txt1 += '</div>';
+    txt1 += '</form>';
+	
+    $("#trabajoCentro").html(txt1); 
+
+    var txt2 = '<div>';
+    txt2 += '	<button id="btnAgregar" class="btn" type="button">AÑADIR</button>';
+    txt2 += ' </div>';
+
+    $("#trabajoSur").html(txt2); 
+}
+
+  
   
     
     
