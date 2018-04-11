1f41f68215cc89f80de3c53ebd3e97de11f4469f
hacu07 <32797963+hacu07@users.noreply.github.com>
2018-04-11 07:26:42 -0500

    grafica linea

--- JS/moduloAdmin.js
+++ JS/moduloAdmin.js
@@ -348,17 +348,79 @@ function consultarVentasGenerales(){
 
 function crearStringEstadisticas(response){
     var paula = [];
+    /*
     for(var i=0; i < response.length; i++) {
         paula.push({"name" : response[i]["nombre"], "data" : [0,parseInt(response[i]["numeroPedidos"])] })
     }
 
+    graficasEstadisticas(paula);*/
+
+    paula["name"] = 'pedidos';
+    paula["data"] = [['Prueba',0]];
+    for(var i=0; i < response.length; i++) {
+        paula["data"].push([response[i]["nombre"],parseInt(response[i]["numeroPedidos"])]);
+    } 
+    graficasBarras(paula);
 
 
-
-    graficasEstadisticas(paula);
+    
 
 }
 
+
+function graficasBarras(respuesta){
+
+Highcharts.chart('trabajoCentro', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'World\'s largest cities per 2014'
+    },
+    subtitle: {
+        text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+    },
+    xAxis: {
+        type: 'category',
+        labels: {
+            rotation: -45,
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population (millions)'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        pointFormat: 'Population in 2008: <b>{point.y:.1f} millions</b>'
+    },
+    series: [{respuesta,
+       
+
+        dataLabels: {
+            enabled: true,
+            rotation: -90,
+            color: '#000000',
+            align: 'right',
+            format: '{point.y:.1f}', // one decimal
+            y: 10, // 10 pixels down from the top
+            style: {
+                fontSize: '13px',
+                fontFamily: 'Verdana, sans-serif'
+            }
+        }
+    }]
+});
+
+}
 
 function graficasEstadisticas(texto){ 
 
